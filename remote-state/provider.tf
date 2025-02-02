a9e6d46d5dd58676e47aeb53b38a0f3ccb5a0ca6
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2025-02-03 00:26:57 +0100

    terraform

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.84.0"
     }
   }
+
+  backend "s3" {
+    bucket = "wastebucket"
+    key    = "ec2-demo"
+    region = "us-east-1"
+    dynamodb_table = "wastelocking"
+  }
 }
 
 provider "aws" {
